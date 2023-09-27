1d48f6e22dfae72138b75a21a237cbc54ee6dabc
Ju-MINJAE <juminjae98@gmail.com>
2023-09-27 19:05:03 +0900

    230927

--- 02-practice-cmp-start/src/App.js
+++ 02-practice-cmp-start/src/App.js
@@ -28,15 +28,25 @@ function App() {
   return (
     <div>
       <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
+        <img src={keyConceptsImage} alt='Medal badge with a star' />
         <h1>Key React Concepts</h1>
         <p>Selected key React concepts you should know about</p>
       </header>
-      <ul id="concepts">
-        <li className="concept">
-          <img src="TODO: IMAGE" alt="TODO: TITLE" />
-          <h2>TODO: TITLE</h2>
-          <p>TODO: DESCRIPTION</p>
+      <ul id='concepts'>
+        <li className='concept'>
+          <img src={concepts[0].image} alt={concepts[0].title} />
+          <h2>{concepts[0].title}</h2>
+          <p>{concepts[0].description}</p>
+        </li>
+        <li className='concept'>
+          <img src={concepts[1].image} alt={concepts[1].title} />
+          <h2>{concepts[1].title}</h2>
+          <p>{concepts[1].description}</p>
+        </li>
+        <li className='concept'>
+          <img src={concepts[2].image} alt={concepts[2].title} />
+          <h2>{concepts[2].title}</h2>
+          <p>{concepts[2].description}</p>
         </li>
       </ul>
     </div>

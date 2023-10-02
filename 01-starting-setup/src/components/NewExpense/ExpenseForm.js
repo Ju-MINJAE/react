e28e2fb3075b3bbc0c357adcc8364b7931d37740
Ju-MINJAE <juminjae98@gmail.com>
2023-10-02 14:02:04 +0900

    231002

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -31,12 +31,31 @@ const ExpenseFrom = () => {
     //setUserInput({ ...userInput, enterdDate: event.target.value });
   };
 
+  const subminHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
   return (
-    <form>
+    <form onSubmit={subminHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler}></input>
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          ></input>
         </div>
       </div>
       <div className='new-expense__controls'>
@@ -46,6 +65,7 @@ const ExpenseFrom = () => {
             type='number'
             min='0.01'
             step='0.01'
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -57,6 +77,7 @@ const ExpenseFrom = () => {
             type='date'
             min='2019-01-01'
             step='2023-12-31'
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>

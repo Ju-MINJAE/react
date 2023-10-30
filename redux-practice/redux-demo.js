fb3e72ccfd697d7b675fc0860c07db6f1b5252fa
Ju-MINJAE <juminjae98@gmail.com>
2023-10-30 21:39:27 +0900

    231030

---
+++ redux-practice/redux-demo.js
@@ -0,0 +1,27 @@
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+const store = redux.createStore(counterReducer);
+// console.log(store.getState()); // -> {counter:1}
+
+const counterSubscriber = () => {
+  const lastestState = store.getState();
+  console.log(lastestState);
+};
+
+store.subscribe(counterSubscriber);
+store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });

a0f5e3a5748a6aa3ace1c61f61d2466e9e916c82
Ju-MINJAE <juminjae98@gmail.com>
2024-01-02 17:51:41 +0900

    240102

--- 26-typescript/react-ts/src/components/Todos.tsx
+++ 26-typescript/react-ts/src/components/Todos.tsx
@@ -1,10 +1,23 @@
-function Todos() {
+import React, { useContext } from 'react';
+
+import TodoItem from './TodoItem';
+import { TodosContext } from '../store/todos-context';
+import classes from './Todos.module.css';
+
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   return (
-    <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+    <ul className={classes.todos}>
+      {todosCtx.items.map((item) => (
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+        />
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;

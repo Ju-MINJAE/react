60527746621ff046ff982b863fb85330def7a45b
Ju-MINJAE <juminjae98@gmail.com>
2024-02-19 15:52:12 +0900

    ToDoList

--- react-Nomad/react-nomad/src/App.js
+++ react-Nomad/react-nomad/src/App.js
@@ -1,39 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState('');
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
 
-  const onClick = () => setCounter((prev) => prev + 1);
-
-  const onChange = (event) => {
-    setKeyword(event.target.value);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') return;
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
   };
-
-  // useEffect function은 코가 딱 한번만 실행될 수 있도록 보호
-  useEffect(() => {
-    console.log('I run only once.');
-  }, []);
-  useEffect(() => {
-    console.log(`I run when 'keyword' changes.`);
-  }, [keyword]);
-  useEffect(() => {
-    console.log(`I run when 'counter' changes.`);
-  }, [counter]);
-  useEffect(() => {
-    console.log(`I run when 'keyword' & 'counter' changes.`);
-  }, [keyword, counter]);
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type='text'
-        placeholder='Search here'
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type='text'
+          placeholder='Write your to do...'
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }

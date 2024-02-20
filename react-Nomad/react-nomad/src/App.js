0157c49da4d51cf4d2988526cb6fbb7e4e86d497
Ju-MINJAE <juminjae98@gmail.com>
2024-02-20 13:51:29 +0900

    240220

--- react-Nomad/react-nomad/src/App.js
+++ react-Nomad/react-nomad/src/App.js
@@ -1,37 +1,21 @@
-import { useState } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/movie',
+    element: <Detail />,
+  },
+]);
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') return;
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo('');
-  };
-
-  return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type='text'
-          placeholder='Write your to do...'
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;

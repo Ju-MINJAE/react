21153279638b09d6900f0ba612194c539c7aef73
Ju-MINJAE <juminjae98@gmail.com>
2024-02-21 14:53:44 +0900

    nwitter

---
+++ nwitter/src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA7wTAVl42cojAHPwdS7jPvDj1hRkCsvXk',
+  authDomain: 'nwitter-reloaded-820c3.firebaseapp.com',
+  projectId: 'nwitter-reloaded-820c3',
+  storageBucket: 'nwitter-reloaded-820c3.appspot.com',
+  messagingSenderId: '747849105093',
+  appId: '1:747849105093:web:0a95ae28445cf7bd72faac',
+};
+
+const app = initializeApp(firebaseConfig);
